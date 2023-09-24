80ec129f3399757dd8bfa5195b058c4f96d6a40b
alfredosalzillo <noctisbels@gmail.com>
2023-09-24 13:33:08 +0200

    fix: fallback title when metadata not available

--- plugins/api/addLink.ts
+++ plugins/api/addLink.ts
@@ -4,13 +4,24 @@ import urlMetadata from 'url-metadata';
 import currentUserInfo from '@/plugins/api/current-user-info';
 import getServerClient from './get-server-client';
 
+const urlTitleMetadata = async (url: string) => {
+  const defaultTitle = new URL(url).hostname;
+  try {
+    const { title } = await urlMetadata(url);
+    return title ?? defaultTitle;
+  } catch (e) {
+    console.error(`impossible to fetch metadata for ${url}`, e);
+  }
+  return defaultTitle;
+};
+
 const addLink = async (url: string) => {
   const client = getServerClient();
   const user = await currentUserInfo(client);
-  const metadata = await urlMetadata(url);
+  const name = await urlTitleMetadata(url);
   const { error } = await client.from('link').insert({
     url,
-    name: metadata.title as string ?? '',
+    name,
     active: true,
     user_id: user.id,
   });
