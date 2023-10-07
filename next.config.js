f9b23434feb6d2c52e9726883cc7f71665b39621
alfredosalzillo <noctisbels@gmail.com>
2023-10-07 18:39:15 +0200

    fix: disabled experiment optimize css for the 'missing critter' error

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
     reactStrictMode: false,
     experimental: {
         serverActions: true,
-        optimizeCss: true,
         optimizePackageImports: ['@mui/material', '@mui/icons-material'],
     }
 }
